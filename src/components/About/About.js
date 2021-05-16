3e61a86c5db90b772c39e14d74e8c6a598e5c761
Valentina Peric <valentinaperic24@gmail.com>
2021-05-16 19:01:39 -0400

    updated my about me

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -11,14 +11,13 @@ const About = () => (
       <div className="about-me-bio">
         <h3>I began coding when I was in middle school.</h3>
 
-        I did not realize that I could make it into a career until I got to college.
+        <p>I did not realize that I could make it into a career until I got to college.
         There was a lot I had to relearn and I realized a lot has changed. This is something that excited me.
-        In the world of technology, one learns quickly that the industry moves <i>quickly.</i>
+        In the world of technology, one learns quickly that the industry moves <i>quickly.</i></p>
 
-        So, this is how the journey of forever being a student started.
-        I attained my degree in computer science in 2016 and I have been coding ever since.
+        <p>So, this is how the journey of forever being a student started.
+        I attained my degree in computer science in 2016 and I have been coding ever since.</p>
 
-        I am currently full stack software engineer.
         <p class="interests"> In a nutshell, I am a community driven engineer who enjoys making the web intuitive,
         accessible, secure, and fast.
         </p>
